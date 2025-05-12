4c939f43fb065b1093ec70b343ef67ac27b63e85
ElvisNdunguMbuthia <maishamagicast@gmail.com>
2025-05-12 21:17:14 +0300

    passed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+function App() {
+    const[dogPics,setDogPics]= useState(null);
+
+    useEffect(()=>{
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(r=>r.json())
+        .then(data=> setDogPics(data.message))
+
+    },[])
+
+    if (!dogPics){
+        return <p>Loading...</p>
+    }
+    return(
+        <div>
+            <img src={dogPics} alt='A Random Dog' title='dog-pic'></img>
+        </div>
+    )
+}
+
+export default App;
